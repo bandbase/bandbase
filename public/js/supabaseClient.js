ffb9dd8c1d14fe9c1efd875a916231b67c3989fb
mattjqueen <mattjqueen@gmail.com>
2025-08-02 17:28:59 -0400

    update for clientside access to vars

--- public/js/supabaseClient.js
+++ public/js/supabaseClient.js
@@ -1,10 +1,10 @@
-// supabaseClient.js
+// In: /public/js/supabaseClient.js
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// These variables are exposed by Vercel to the browser
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+// Read the configuration that was rendered into the HTML by the server
+const supabaseUrl = window.SUPABASE_CONFIG.url;
+const supabaseAnonKey = window.SUPABASE_CONFIG.key;
 
 // Create and export the Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
