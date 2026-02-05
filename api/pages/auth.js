c1fbc5965c1868ceeb6f57154ae7c1735592c551
mattjqueen <mattjqueen@gmail.com>
2026-02-04 19:01:07 -0500

    reuse for james frontdoor

--- api/pages/auth.js
+++
@@ -1,7 +0,0 @@
-// /api/pages/auth.js
-
-import { renderPage } from '../../lib/ssr-helpers.js';
-
-export default async function handler(request, response) {
-  await renderPage(response, 'auth.ejs');
-}
--- public/js/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-// /public/js/supabaseClient.js
-
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const supabaseUrl = window.SUPABASE_CONFIG.url;
-const supabaseAnonKey = window.SUPABASE_CONFIG.key;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
